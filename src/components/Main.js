4f27581d887aeff037cf4ca1ee35d37c7e518f99
Jason Kulak <JasonKulak@yahoo.com>
2024-01-26 16:48:51 -0600

    Imported projects into main jsx

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
+import Projects from "./Projects.js"
+
 // the component function
 const Main = (props) => {
 
     //The Components Returned JSX
-    return <div className="Main"> This is Main</div>
+    return <div className="Main"> <Projects/></div>
 }
 
 // export the component
