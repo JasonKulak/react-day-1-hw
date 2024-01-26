9d67f3d052f4ab9420d3acd81e0f99347f58c9f9
Jason Kulak <JasonKulak@yahoo.com>
2024-01-26 16:17:19 -0600

    github and LinkedIn links in browser and spaced evenly

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import './index.css'
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header /> 
+        <div className="links">
+          <a href="https://github.com/JasonKulak">Jason's github</a>
+          <a href="https://www.linkedin.com/in/jason-kulak-2330b42a4/">Jason's LinkedIn</a>
+        </div>
       <Main />
       <Footer />
     </div>
