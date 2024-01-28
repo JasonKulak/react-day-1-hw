61b8282c9acd111709fd0565b94bee4dcb2c4345
Jason Kulak <JasonKulak@yahoo.com>
2024-01-28 10:20:12 -0600

    footer finished and in position at bottom of page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,19 @@
 const Footer = (props) => {
 
     //The Components Returned JSX
-    return <div className="Footer"> This is Footer</div>
+    return <div className="Footer"> This is Footer
+
+        <div className="boxes">
+            <div className="Flexbox">
+                <h3>Need help with a project?</h3>
+                 <h4> - JK Dev Design - </h4>
+            </div>
+            <div className="Bottom">
+                 <h5>jkulak84@gmail.com</h5>
+            </div>
+        </div>
+
+    </div>
 }
 
 // export the component
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,12 +3,13 @@ const Projects = (props) => {
 
     //The Components Returned JSX
     return <div className="Projects"> This is Projects
-        <div className="project-one">
+        <div className="project-one-card">
             Star Wars Trivia Game
             <a href="https://github.com/JasonKulak/Seal_Project1">github</a>
             <a href="https://jasonkulak.github.io/Seal_Project1/">Trivia site</a>
+            
         </div>
-        <div className="project-two">
+        <div className="project-two-card">
             World Brunches
             <a href="https://github.com/JasonKulak/Project_2">github</a>
             <a href="https://jk-project-2.onrender.com/sandwich">Brunch site</a>
